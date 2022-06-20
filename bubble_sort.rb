4663657d72658fa3b77a37aef399d5be719e5565
humanathome <eabicic@gmail.com>
2022-06-20 13:33:34 +0200

    add second solution

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,5 +13,23 @@ def bubble_sort(input_array)
   input_array
 end
 
+# solution using a boolean variable as a condition
+def bubble_sort(input_array)
+  return input_array if input_array.length <= 1
+  swapped = true
+
+  while swapped
+    swapped = false
+    # .times loop starts at 0, so iterator block variable current_position serves as an index of the current element in the array
+    (input_array.length - 1).times do |current_position|
+      if input_array[current_position] > input_array[current_position + 1]
+        input_array[current_position], input_array[current_position + 1] = input_array[current_position + 1], input_array[current_position]
+        swapped = true
+      end
+    end
+  end
+  input_array
+end
+
 p bubble_sort([6, 5, 3, 1, 8, 7, 2, 4])
 p bubble_sort([4, 3, 5, 6, 7])
