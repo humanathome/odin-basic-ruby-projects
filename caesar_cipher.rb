d154bba17531558ea46511d17996257f32d24f23
humanathome <eabicic@gmail.com>
2022-06-23 15:51:47 +0200

    remove redundant method calls on the block variable

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,9 +2,9 @@ def caesar_cipher(sentence, places)
 
   shifted = sentence.codepoints.map do |character_code|
     if character_code.between?(97, 122)
-      (((character_code.ord - 97 + places) % 26) + 97).chr
+      (((character_code - 97 + places) % 26) + 97).chr
     elsif character_code.between?(65, 90)
-      (((character_code.ord - 65 + places) % 26) + 65).chr
+      (((character_code - 65 + places) % 26) + 65).chr
     else
       character_code
     end
